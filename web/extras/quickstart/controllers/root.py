e728284362a0886499073ab6e47896273e2c5ed3
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-11-15 19:48:59 -0800

    More large-scale updates.

--- web/extras/quickstart/controllers/root.py
+++ web/extras/quickstart/controllers/root.py
@@ -5,6 +5,7 @@ import web
 from web.extras.quickstart import model
 
 from web.extras.quickstart.controllers.metadata import MetadataController
+from web.extras.quickstart.controllers.scm import SCMController
 
 
 __all__ = ['RootController']
@@ -15,6 +16,7 @@ log = __import__('logging').getLogger(__name__)
 class RootController(web.core.Controller):
     # Overview Sub-Items
     metadata = MetadataController()
+    scm = SCMController()
     
     def index(self):
         return 'web.extras.quickstart.templates.index', dict()
